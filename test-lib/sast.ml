f3135e5b5a2047f9dafdcbfac4fa4b697c6de57a
Avi Chad-Friedman <ajc2212@columbia.edu>
2015-11-13 16:00:05 -0500

    print types in sast

--- test-lib/sast.ml
+++ test-lib/sast.ml
@@ -16,14 +16,18 @@ type statement =
 	stmt_detail * t
 
 
+let string_of_type = function
+	Int -> " Int"
+
 let rec expr = function
-	Ast.IntLit( c ) -> print_int c; IntLit( c ), Int
+	Ast.IntLit( c ) -> print_int c; print_endline (string_of_type Int); IntLit( c ), Int
 
 let rec stmt = function
 	Ast.Expr( e ) -> Expr( expr e )
 
 let program program = 
 	List.map stmt program
+
 
 
 
