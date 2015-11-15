eda7a191b2d587eb7db4347a7c65711ee2805a5b
evanoconnor <eco2116@columbia.edu>
2015-11-15 18:47:23 -0500

    Added logging to test suite and renamed to .cpp extensions

--- test-lib/reg_test.sh
+++ test-lib/reg_test.sh
@@ -1,6 +1,55 @@
 #!/bin/bash
+echo
+echo ====================================================
+echo ===============ACCELERATOR TEST SUITE===============
+echo ====================================================
+echo
+echo
+echo ====================================================
+echo Cleaning and making...
+echo ====================================================
+echo
 make clean && make
-./acc < addTest.acc > addTest.c
-diff addTest.c addTestExpected.c
+echo
+echo ====================================================
+echo Running Tests...
+echo ====================================================
+echo
 
+./acc < addTest.acc > addTest.cpp
+
+declare -i count
+declare -i failures
+count=0
+failures=0
+
+count+=1
+echo Running test: $count
+echo
+if diff "addTest.cpp" "addTestExpected.cpp" > /dev/null; then
+	echo Test $count passed
+else
+	echo Test $count failed
+	failurs+=1
+fi
+
+echo
+echo Accelerator input:
+cat addTest.acc
+echo
+echo Expected C++ output:
+cat addTestExpected.cpp
+echo
+echo Actual C++ output:
+cat addTest.cpp
+echo
+
+echo ====================================================
+echo Results
+echo ====================================================
+echo Test Suite finished
+echo Failed Tests: $failures
+echo Total Tests: $count
+echo
+echo Cleaning up
 make clean
