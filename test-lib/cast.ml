baffc709845f9f7fba12fc095b22e3f7f53b49c7
Avi Chad-Friedman <ajc2212@columbia.edu>
2015-12-18 17:38:16 -0500

    sast compiles again

--- test-lib/cast.ml
+++ test-lib/cast.ml
@@ -103,87 +103,87 @@ let string_of_id = function
   Id(s) -> s 
 
 let rec type_match = function
-  | Environment.String -> String
-  | Environment.Int -> Int
-  | Environment.Float -> Float
-  | Environment.Bool -> Bool
-  | Environment.Na -> Void
-  | Environment.IdType -> IdType
-  | Environment.Vector(t) -> Vector(type_match t)
-  | Environment.Matrix(t) -> Vector(type_match t)
+  | Sast.String -> String
+  | Sast.Int -> Int
+  | Sast.Float -> Float
+  | Sast.Bool -> Bool
+  | Sast.Na -> Void
+  | Sast.IdType -> IdType
+  | Sast.Vector(t) -> Vector(type_match t)
+  | Sast.Matrix(t) -> Vector(type_match t)
 
 let id_match = function
-  | Environment.Id(s) -> Id(s)
+  | Sast.Id(s) -> Id(s)
 
 let rec cexpr_detail = function
- | Environment.IdLit(s, t) ->  IdLit(s, type_match t)
- | Environment.IntLit(i) -> IntLit(i)
- | Environment.FloatLit(i) -> FloatLit(i)
- | Environment.BoolLit(b) -> BoolLit(b)
- | Environment.StringLit(s) -> StringLit(s)
- | Environment.IntExpr(e,t) -> IntExpr(cexpr_detail e, type_match t)
- | Environment.Vector(s, v, t) -> let ct = type_match t in
+ | Sast.IdLit(s, t) ->  IdLit(s, type_match t)
+ | Sast.IntLit(i) -> IntLit(i)
+ | Sast.FloatLit(i) -> FloatLit(i)
+ | Sast.BoolLit(b) -> BoolLit(b)
+ | Sast.StringLit(s) -> StringLit(s)
+ | Sast.IntExpr(e,t) -> IntExpr(cexpr_detail e, type_match t)
+ | Sast.Vector(s, v, t) -> let ct = type_match t in
          Vector(s, List.map cexpr_detail v, ct)
- | Environment.VectIdAcc(e1, e2, t) ->
+ | Sast.VectIdAcc(e1, e2, t) ->
         VectIdAcc(e1, e2, type_match t)
- | Environment.VectIntAcc(e1, e2, t) ->
+ | Sast.VectIntAcc(e1, e2, t) ->
         VectIntAcc(e1, cexpr_detail e2, type_match t) 
- | Environment.NaExpr(t) -> Na("Void", type_match t)
- | Environment.Matrix(s, v, nr, nc, t) -> let ct = type_match t in
+ | Sast.NaExpr(t) -> Na("Void", type_match t)
+ | Sast.Matrix(s, v, nr, nc, t) -> let ct = type_match t in
         Matrix(s, List.map cexpr_detail v, cexpr_detail nr, cexpr_detail nc, ct)
- | Environment.MatrixIdAcc(s, id1, id2, t) ->
+ | Sast.MatrixIdAcc(s, id1, id2, t) ->
         MatrixIdAcc(s, id1, id2, type_match t)
- | Environment.MatrixIntAcc(s, i1, i2, t) ->
+ | Sast.MatrixIntAcc(s, i1, i2, t) ->
         MatrixIntAcc(s, cexpr_detail i1, cexpr_detail i2, type_match t)
  (*Expand when you pull in Alan's Fadd etc.*)
- | Environment.Add(e1, e2, t) -> Add((cexpr_detail e1), (cexpr_detail e2), Int)
- | Environment.Sub(e1, e2, t) -> Sub(cexpr_detail e1, cexpr_detail e2, Int)
- | Environment.Mult(e1, e2, t) -> Mult(cexpr_detail e1, cexpr_detail e2, Int)
- | Environment.Div(e1, e2, t) -> Div(cexpr_detail e1, cexpr_detail e2, Int)
- | Environment.FAdd(e1, e2, t) -> Add((cexpr_detail e1), (cexpr_detail e2), Float)
- | Environment.FSub(e1, e2, t) -> Sub(cexpr_detail e1, cexpr_detail e2, Float)
- | Environment.FMult(e1, e2, t) -> Mult(cexpr_detail e1, cexpr_detail e2, Float)
- | Environment.FDiv(e1, e2, t) -> Div(cexpr_detail e1, cexpr_detail e2, Float)
- | Environment.Expo(e1, e2, t) -> Expo(cexpr_detail e1, cexpr_detail e2, Int)
- | Environment.Mod(e1, e2, t) -> Mod(cexpr_detail e1, cexpr_detail e2, Int)
- | Environment.FuncCall(id, el, t) -> let ct = type_match t in
+ | Sast.Add(e1, e2, t) -> Add((cexpr_detail e1), (cexpr_detail e2), Int)
+ | Sast.Sub(e1, e2, t) -> Sub(cexpr_detail e1, cexpr_detail e2, Int)
+ | Sast.Mult(e1, e2, t) -> Mult(cexpr_detail e1, cexpr_detail e2, Int)
+ | Sast.Div(e1, e2, t) -> Div(cexpr_detail e1, cexpr_detail e2, Int)
+ | Sast.FAdd(e1, e2, t) -> Add((cexpr_detail e1), (cexpr_detail e2), Float)
+ | Sast.FSub(e1, e2, t) -> Sub(cexpr_detail e1, cexpr_detail e2, Float)
+ | Sast.FMult(e1, e2, t) -> Mult(cexpr_detail e1, cexpr_detail e2, Float)
+ | Sast.FDiv(e1, e2, t) -> Div(cexpr_detail e1, cexpr_detail e2, Float)
+ | Sast.Expo(e1, e2, t) -> Expo(cexpr_detail e1, cexpr_detail e2, Int)
+ | Sast.Mod(e1, e2, t) -> Mod(cexpr_detail e1, cexpr_detail e2, Int)
+ | Sast.FuncCall(id, el, t) -> let ct = type_match t in
                                FuncCall(id, 
                                List.map cexpr_detail el, ct)
- | Environment.Assign(id, e, t) -> let ct = type_match t in
+ | Sast.Assign(id, e, t) -> let ct = type_match t in
                              Assign(id, cexpr_detail e, ct)
- | Environment.And(e1, e, t) -> let ct = type_match t in 
+ | Sast.And(e1, e, t) -> let ct = type_match t in 
                           And(cexpr_detail e1, cexpr_detail e, ct)
- | Environment.Or(e1, e2, t) ->  let ct = type_match t in
+ | Sast.Or(e1, e2, t) ->  let ct = type_match t in
                           Or(cexpr_detail e1, cexpr_detail e2, ct)
- | Environment.Not(e, t) -> let ct = type_match t in
+ | Sast.Not(e, t) -> let ct = type_match t in
                       Not(cexpr_detail e, ct)
 
 let rec cexpr = function
-  | Environment.Sexpr(e, t) -> Cexpr(cexpr_detail e, type_match t)
-  | Environment.Sadd(e1, e2, t) -> Cadd(cexpr e1, cexpr e2, type_match t)
-  | Environment.Ssub(e1, e2, t) -> Csub(cexpr e1, cexpr e2, type_match t)
-  | Environment.Smult(e1, e2, t) -> Cmult(cexpr e1, cexpr e2, type_match t)
-  | Environment.Sdiv(e1, e2, t) -> Cdiv(cexpr e1, cexpr e2, type_match t)
-  | Environment.SFAdd(e1, e2, t) -> Cadd(cexpr e1, cexpr e2, type_match t)
-  | Environment.SFSub(e1, e2, t) -> Csub(cexpr e1, cexpr e2, type_match t)
-  | Environment.SFMult(e1, e2, t) -> Cmult(cexpr e1, cexpr e2, type_match t)
-  | Environment.SFDiv(e1, e2, t) -> Cdiv(cexpr e1, cexpr e2, type_match t)
-  | Environment.Sexpo(e1, e2, t) -> Cexpo(cexpr e1, cexpr e2, type_match t)
-  | Environment.Smod(e1, e2, t) -> Cmod(cexpr e1, cexpr e2, type_match t)
-  | Environment.SfuncCall(el, t) -> CfuncCall((List.map cexpr el), type_match t)
-  | Environment.Sassign(e, t) -> Cassign(cexpr e, type_match t)
-  | Environment.Sand(e1, e2, t) -> Cand(cexpr e1, cexpr e2, type_match t)
-  | Environment.Sor(e1, e2, t) -> Cor(cexpr e1, cexpr e2, type_match t)
-  | Environment.Snot(e, t) -> Cnot(cexpr e, type_match t)
+  | Sast.Sexpr(e, t) -> Cexpr(cexpr_detail e, type_match t)
+  | Sast.Sadd(e1, e2, t) -> Cadd(cexpr e1, cexpr e2, type_match t)
+  | Sast.Ssub(e1, e2, t) -> Csub(cexpr e1, cexpr e2, type_match t)
+  | Sast.Smult(e1, e2, t) -> Cmult(cexpr e1, cexpr e2, type_match t)
+  | Sast.Sdiv(e1, e2, t) -> Cdiv(cexpr e1, cexpr e2, type_match t)
+  | Sast.SFAdd(e1, e2, t) -> Cadd(cexpr e1, cexpr e2, type_match t)
+  | Sast.SFSub(e1, e2, t) -> Csub(cexpr e1, cexpr e2, type_match t)
+  | Sast.SFMult(e1, e2, t) -> Cmult(cexpr e1, cexpr e2, type_match t)
+  | Sast.SFDiv(e1, e2, t) -> Cdiv(cexpr e1, cexpr e2, type_match t)
+  | Sast.Sexpo(e1, e2, t) -> Cexpo(cexpr e1, cexpr e2, type_match t)
+  | Sast.Smod(e1, e2, t) -> Cmod(cexpr e1, cexpr e2, type_match t)
+  | Sast.SfuncCall(el, t) -> CfuncCall((List.map cexpr el), type_match t)
+  | Sast.Sassign(e, t) -> Cassign(cexpr e, type_match t)
+  | Sast.Sand(e1, e2, t) -> Cand(cexpr e1, cexpr e2, type_match t)
+  | Sast.Sor(e1, e2, t) -> Cor(cexpr e1, cexpr e2, type_match t)
+  | Sast.Snot(e, t) -> Cnot(cexpr e, type_match t)
 
 let rec stmt = function
-  | Environment.Sstmt(e, t) -> let r = cexpr e in
+  | Sast.Sstmt(e, t) -> let r = cexpr e in
                         Cstmt(r, type_match t)
-  | Environment.Sblock(sl, t) -> let l = List.map stmt sl in
+  | Sast.Sblock(sl, t) -> let l = List.map stmt sl in
                         Cblock(l, type_match t)
-  | Environment.Sif(e, s1, s2, t) -> let r = cexpr e in
+  | Sast.Sif(e, s1, s2, t) -> let r = cexpr e in
                               Cif(r, stmt s1, stmt s2, type_match t)
-  | Environment.Sfor(id, e1, e2, s, t) -> Cfor(id, cexpr e1, cexpr e2, stmt s, Void)
+  | Sast.Sfor(id, e1, e2, s, t) -> Cfor(id, cexpr e1, cexpr e2, stmt s, Void)
  
  (*return a c program in the form of a single *)
 let program cast = 
